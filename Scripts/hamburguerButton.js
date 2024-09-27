51ff85ed70414db8afe5a06027f29b937f2dda26
LucasMorais16 <lucas.santoslima.morais@hotmail.com>
2024-09-26 22:33:36 -0300

    adicionado animação no header.
    - Animação no header ao usar o scroll para descer e subir a página.
    - Ajustando o tamanho da caixa do dropdown.

--- Scripts/hamburguerButton.js
+++ Scripts/hamburguerButton.js
@@ -4,15 +4,27 @@ const hamburger = document.querySelector(".menu-toggle");
 const closeIcon = document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
 
+let menuIsShown;
+
+function shouldToggle() {
+    if(menuIsShown == true){
+        toggleMenu();
+    }else{
+        return;
+    }
+}
+
 function toggleMenu() {
     if (menu.classList.contains("showMenu")) {
         menu.classList.remove("showMenu");
         closeIcon.style.display = "none";
         menuIcon.style.display = "block";
+        menuIsShown = false;
     } else {
         menu.classList.add("showMenu");
         closeIcon.style.display = "block";
         menuIcon.style.display = "none";
+        menuIsShown = true;
     }
 }
 
@@ -20,6 +32,6 @@ hamburger.addEventListener("click", toggleMenu);
 
 menuItems.forEach(
     function (menuItem) {
-        menuItem.addEventListener("click", toggleMenu);      
+        menuItem.addEventListener("click", shouldToggle);              
     }
 )
